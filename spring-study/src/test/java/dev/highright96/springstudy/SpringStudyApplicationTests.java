3978f4899f6df8f6e30bb7f27bc8e0afb887b477
highright96 <nsw5296@naver.com>
2022-01-23 20:02:54 +0900

    chore: 설정 파일 추가

---
+++ spring-study/src/test/java/dev/highright96/springstudy/SpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.highright96.springstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
